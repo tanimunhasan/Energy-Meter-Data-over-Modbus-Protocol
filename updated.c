53002e36e1d4aabc6ef9b71cc1b50db64740f2a3
Tanimun Hasan <59294968+tanimunhasan@users.noreply.github.com>
2022-06-08 17:16:13 +0600

    code modified

---
+++ updated.c
@@ -0,0 +1,83 @@
+#include <ModbusMaster.h>                //need to include Modbus Master library
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(A3, A2);
+
+#define MAX485_DE      A4
+#define MAX485_RE_NEG  A5
+
+// instantiate ModbusMaster object
+ModbusMaster node;
+word CRC16 (const byte *nData, word wLength);
+
+void preTransmission()
+{
+  digitalWrite(MAX485_RE_NEG, 1);
+  digitalWrite(MAX485_DE, 1);
+}
+
+void postTransmission()
+{
+  digitalWrite(MAX485_RE_NEG, 0);
+  digitalWrite(MAX485_DE, 0);
+}
+
+void setup()
+{
+  pinMode(MAX485_RE_NEG, OUTPUT);
+  pinMode(MAX485_DE, OUTPUT);
+  // Init in receive mode
+  digitalWrite(MAX485_RE_NEG, 0);
+  digitalWrite(MAX485_DE, 0);
+
+  // Modbus communication runs at 115200 baud
+  Serial.begin(9600);
+  mySerial.begin(9600);
+
+  // Modbus slave ID 1
+  node.begin(2, mySerial);
+  // Callbacks allow us to configure the RS485 transceiver correctly
+  node.preTransmission(preTransmission);
+  node.postTransmission(postTransmission);
+}
+
+bool state = true;
+
+void loop()
+{
+  uint8_t result,LVA,LVB,LVC,LCA,LCB,LCC;
+
+  uint16_t data[6];
+
+  // // Toggle the coil at address 0x0002 (Manual Load Control)
+  // result = node.writeSingleCoil(0x0002, state);
+  // state = !state;
+
+  // Read 16 registers starting at 0x3100)
+  result = node.readHoldingRegisters(0x0061, 30);
+  LVA = node.getResponseBuffer(0x00) / 10.0f;
+  LVB = node.getResponseBuffer(0x01) / 10.0f;
+  LVC = node.getResponseBuffer(0x02) / 10.0f;
+  LCA = node.getResponseBuffer(0x03) / 100.0f;
+  LCB = node.getResponseBuffer(0x04) / 100.0f;
+  LCC = node.getResponseBuffer(0x05) / 100.0f;
+  if (result == node.ku8MBSuccess)
+  {
+    Serial.print("Phase A Voltage: ");
+    Serial.println(LVA);
+    Serial.print("Phase B Voltage: ");
+    Serial.println(LVB);
+    Serial.print("Phase C Voltage: ");
+    Serial.println(LVC);
+
+    Serial.print("Phase A Current: ");
+    Serial.println(LCA);
+    Serial.print("Phase B Current: ");
+    Serial.println(LCB);
+    Serial.print("Phase C Current: ");
+    Serial.println(LCC);
+
+  }
+
+  delay(1000);
+}
